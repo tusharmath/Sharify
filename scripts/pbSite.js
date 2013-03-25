601eebaf3ace9ed2087dbc9809e891365d1406f1
Tushar Mathur <tusharmath@gmail.com>
2013-03-25 20:58:51 +0530

    Adding default  test case to app

--- scripts/pbSite.js
+++ scripts/pbSite.js
@@ -1,6 +1,6 @@
 var element = $("div.commandBox textarea");
 element.focus();
-element.val("a paid 4400 for a, d, w ,s # were woloves");
+element.val("Paid 5000 \nby A, B * 2, C \nfor A, B * 2, C * 2, D, E * 2, F * 2, G, I \n#Tagname : remarks");
 //var formatter = new pbFormat(element);
 
 var parser = new pbParser(element);
