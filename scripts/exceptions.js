3873aa173db1710617563dbaa97fbb2c9fb5f740
Tushar Mathur <tusharmath@gmail.com>
2013-03-25 20:22:15 +0530

    Adding exceptions script

---
+++ scripts/exceptions.js
@@ -0,0 +1,8 @@
+var NotImplementedException = function() {
+	return "Method has not been implemnted!";
+};
+
+var InvalidParameterTypeExecption = function() {
+
+	return "An invalid type of paramter was suppied to the calling method!";
+};
