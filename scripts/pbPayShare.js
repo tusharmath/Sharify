89de864ae77bf982481edf45b26dace2a7af00c9
Tushar Mathur <tusharmath@gmail.com>
2013-03-25 19:26:15 +0530

    pbPayShare converts all ratios to values

--- scripts/pbPayShare.js
+++ scripts/pbPayShare.js
@@ -1,22 +1,25 @@
 var pbPayShare = function(pbShareList, amount) {
 
+	var pbPayShareItem = function(user, amount) {
+		return {
+			user: user,
+			amount: amount
+		};
+	};
+
 	var _shareList = [];
-	if (amount === null) {
-		_shareList = pbShareList;
-	} else {
-		var _totalShare = 0;
-		for (var i = pbShareList.length - 1; i >= 0; i--) {
-			_totalShare += pbShareList[i].share;
-		}
+	var _totalShare = 0;
+	pbShareList.foreach(function(p) {
+		_totalShare += p.share;
+	});
+
+	pbShareList.foreach(function(p) {
+
+		var x = new pbPayShareItem(s.user, amount * s.share / _totalShare);
+		_shareList.push(x);
+	});
 
 
-		for (var j = pbShareList.length - 1; j >= 0; j--) {
-			var s = pbShareList[j];
-			var n = new pbShare(s.user, amount * s.share / _totalShare);
-			_shareList.push(n);
-		}
-
-	}
 
 	return _shareList;
 
