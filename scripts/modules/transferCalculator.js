4d08e18611bc731f92b0838190ff415975c81872
Tushar Mathur <tusharmath@gmail.com>
2013-03-30 02:24:50 +0530

    adding basic code for permutations

--- scripts/modules/transferCalculator.js
+++ scripts/modules/transferCalculator.js
@@ -1,5 +1,47 @@
 var transferCalculator = function(balances) {
 
+
+	var swap = function(z, l, j) {
+		var e = z.slice(0);
+		var k = e[l];
+		e[l] = e[j];
+		e[j] = k;
+		return e;
+	};
+	var _permu = function(p, index) {
+		log(p);
+
+		for (var i = index; i >= 0; i--) {
+			for (var j = 0; j < i; j++) {
+				var t = swap(p, j, i);
+
+				_permu(t, i - 1);
+			}
+		}
+
+
+	};
+
+	var startPerm = function(q) {
+		count = 0;
+		finalr.length = 0;
+		_permu(q, q.length - 1);
+		console.log(finalr);
+	};
+	var _factorial = function(i) {
+		var fact = i;
+		while (i !== 1) {
+			fact *= --i;
+		}
+		return fact;
+	};
+
+
+	var _getPermutations = function(elements) {
+
+
+	};
+
 	var _getDuplexes = function(balances) {
 		var _duplexes = [];
 
@@ -40,3 +82,12 @@ var transferCalculator = function(balances) {
 2. Iterate over all permutations  of the duplex combinations.
 
 		*/
+
+var count = 0;
+var finalr = [];
+var log = function(e) {
+	//console.log(count, "-", e.join(""));
+	finalr.push(e);
+	//count++;
+
+};
