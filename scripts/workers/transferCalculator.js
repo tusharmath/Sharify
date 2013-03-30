c228c7c2fa94d5fd061b5eecdec7e48c04aae735
Tushar Mathur <tusharmath@gmail.com>
2013-03-30 14:42:04 +0530

    Finally !!!! Transfers created

--- scripts/workers/transferCalculator.js
+++ scripts/workers/transferCalculator.js
@@ -55,8 +55,8 @@ var transferCalculator = function(balances) {
 			for (var j = payees.length - 1; j >= 0; j--) {
 				var payee = payees[j];
 				_duplexes.push({
-					from: payee,
-					to: payer
+					from: payee.user,
+					to: payer.user
 				});
 			}
 		}
@@ -64,8 +64,116 @@ var transferCalculator = function(balances) {
 
 	};
 
+	var transfers = function() {
+		_transfers = [];
+		var _add = function(from, to, amount) {
+			_transfers.push({
+				from: from,
+				to: to,
+				amount: amount
+			});
+
+
+
+		};
+
+		var _list = function() {
+			return _transfers;
+		};
+
+		return {
+			add: _add,
+			list: _list
+		};
+	};
+	//TODO:Think of a suitable name for this function
+	var _transferBuilder = function(perm, balanceKeys) {
+		var from = 0;
+		var to = 0;
+		var bKeys = {};
+		for (var i in balanceKeys) {
+			bKeys[i] = balanceKeys[i];
+		}
+
+		var t = new transfers();
+		//FROM: -ve & TO: +ve
+		do {
+
+			if (bKeys[perm[from].from] === 0) {
+				from++;
+			} else if (bKeys[perm[to].to] === 0) {
+				to++;
+			} else if (bKeys[perm[from].from] + bKeys[perm[to].to] === 0) {
+				t.add(perm[from].from, perm[to].to, bKeys[perm[to].to]);
+
+				bKeys[perm[from].from] = 0;
+				bKeys[perm[to].to] = 0;
+
+				from++;
+				to++;
+
+			} else if (bKeys[perm[from].from] + bKeys[perm[to].to] > 0) {
+				t.add(perm[from].from, perm[to].to, -bKeys[perm[from].from]);
+
+				bKeys[perm[to].to] += bKeys[perm[from].from];
+				bKeys[perm[from].from] = 0;
+
+
+				from++;
+
+			} else if (bKeys[perm[from].from] + bKeys[perm[to].to] < 0) {
+				t.add(perm[from].from, perm[to].to, bKeys[perm[to].to]);
+
+				bKeys[perm[from].from] += bKeys[perm[to].to];
+				bKeys[perm[to].to] = 0;
+
+
+				to++;
+
+			}
+
+
+
+		}
+		while (from != perm.length && to != perm.length);
+
+		return t.list();
+
+	};
+
+
+
+	var _getTransfers = function(balances) {
+		var perms = _getPermutations(balances);
+		var bKeys = [];
+		balances.forEach(function(p) {
+			bKeys[p.user] = p.amount;
+		});
+		var bal = 0;
+		var i = 0;
+		var minTransfer = perms[0].length;
+		var bestTransfers;
+
+		do {
+			var p = perms[i];
+			var a = _transferBuilder(p, bKeys);
+			if (a.length <= minTransfer) {
+
+
+				if (a.length < minTransfer) {
+					minTransfer = a.length;
+					bestTransfers = a;
+				}
+
+
+			}
+		} while (++i != perms.length);
+
+		return bestTransfers;
+	};
+
 	return {
-		getPermutations: _getPermutations
+		solve: _getTransfers
 	};
 
 };
@@ -74,7 +182,7 @@ var calc = new transferCalculator();
 
 this.addEventListener('message', function(e) {
 
-	var x = calc.getPermutations(e.data);
+	var x = calc.solve(e.data);
 
-	this.postMessage(x.length);
+	this.postMessage(x);
 }, false);
