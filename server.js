91fbea529108521c1dcd7055116c620e2c005344
Tushar Mathur <tusharmath@gmail.com>
2013-04-01 02:20:20 +0530

    node js on server side

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require('express');
 // Path to our public directory
 
 var pub = __dirname + '/src';
+console.log("dir name", __dirname);
 
 // setup middleware
 
@@ -17,27 +18,15 @@ app.use(express.errorHandler());
 
 // Optional since express defaults to CWD/views
 
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/src/views');
 
 // Set our default template engine to "jade"
 // which prevents the need for extensions
 // (although you can still mix and match)
 app.set('view engine', 'jade');
 
-function User(name, email) {
-  this.name = name;
-  this.email = email;
-}
-
-// Dummy users
-var users = [
-    new User('tj', 'tj@vision-media.ca')
-  , new User('ciaran', 'ciaranj@gmail.com')
-  , new User('aaron', 'aaron.heckmann+github@gmail.com')
-];
-
 app.get('/', function(req, res){
-  res.render('users', { users: users });
+  res.render('index');
 });
 
 app.listen(3000);
