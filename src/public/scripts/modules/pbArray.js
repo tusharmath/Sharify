8d2da44ed481c086c4c1eee76de0dd3a9ea67631
Tushar Mathur <tusharmath@gmail.com>
2013-04-03 12:58:24 +0530

    adding pbArray which helps in rotation of an array

---
+++ src/public/scripts/modules/pbArray.js
@@ -0,0 +1,14 @@
+var pbArray = function(arr, count) {
+	var _arr = arr.slice(0, arr.length);
+	count = count === undefined ? 0 : count + 1;
+	var _rotateLeft = function() {
+		if (count == _arr.length) return null;
+		var t = _arr[0];
+		var a = _arr.slice(1, _arr.length);
+		a.push(t);
+		return new pbArray(a, count);
+	};
+
+	_arr.rotateLeft = _rotateLeft;
+	return _arr;
+};
