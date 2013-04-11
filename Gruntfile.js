715e0d4c2647093dc80f49bcd39169b5acc07a5a
Tushar Mathur <tusharmath@gmail.com>
2013-04-12 00:56:11 +0530

    grunt will be minifying css and js now

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,30 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		uglify: {
-			dist: {
-				files: {
-					'public/site.min.js': ['src/scripts/*/*.js']
-				}
+	grunt.initConfig(config);
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.registerTask('default', ['uglify', 'less']);
+};
+
+var config = {
+	//https://github.com/gruntjs/grunt-contrib-uglify
+	uglify: {
+		prod: {
+			files: {
+				'public/site.min.js': ['src/scripts/*/*.js']
 			}
 		}
-	});
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['uglify']);
+	},
+	//https://github.com/gruntjs/grunt-contrib-less
+	less: {
+		prod: {
+			options: {
+				paths: ["src/less/vendor/twitter/bootstrap/less"]
+			},
+			files: {
+				"public/site.min.css": "src/less/style.less"
+			}
+		}
+	}
 };
